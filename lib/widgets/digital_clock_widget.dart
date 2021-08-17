8f746df9765aa429f4c2333c7d217cd888b04058
Shahpasand <ra.shahpasand@gmail.com>
2021-08-17 17:14:43 +0430

    add analog clock

--- lib/widgets/digital_clock_widget.dart
+++ lib/widgets/digital_clock_widget.dart
@@ -1,34 +1,19 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class DigitalClockWidget extends StatefulWidget {
-  @override
-  _DigitalClockWidgetState createState() => _DigitalClockWidgetState();
-}
+class DigitalClockWidget extends StatelessWidget {
+  final TimeOfDay timeOfDay;
 
-class _DigitalClockWidgetState extends State<DigitalClockWidget> {
-  TimeOfDay _timeOfDay = TimeOfDay.now();
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      if (TimeOfDay.now().minute != _timeOfDay.minute) {
-        setState(() => _timeOfDay = TimeOfDay.now());
-      }
-    });
-  }
+  const DigitalClockWidget({Key key, this.timeOfDay}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String _period = _timeOfDay.period == DayPeriod.am ? "AM" : "PM";
+    String _period = timeOfDay.period == DayPeriod.am ? "AM" : "PM";
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          '${_timeOfDay.hourOfPeriod.toString().padLeft(2, '0')} : ${_timeOfDay.minute.toString().padLeft(2, '0')}',
+          '${timeOfDay.hourOfPeriod.toString().padLeft(2, '0')} : ${timeOfDay.minute.toString().padLeft(2, '0')}',
           style: Theme.of(context).textTheme.headline1,
         ),
         SizedBox(width: 5),
